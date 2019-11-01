973bfe09a8fcd505d22f60cf559c274b780d5e21
Alan <alanpauley@gmail.com>
2019-11-01 15:54:30 -0700

    20191101 - Episode 3 (midway)
    Added code, but no comments. I'll get that from our better/old code

--- platforming101/src/dev/apauley/Launcher.java
+++ platforming101/src/dev/apauley/Launcher.java
@@ -5,7 +5,7 @@ import dev.apauley.display.Display;
 public class Launcher {
 
 	public static void main(String[] args) {
-		new Display("Title", 300, 300);
+		new Game("Title", 300, 300);
 	}
 	
 }
--- platforming101/src/dev/apauley/display/Display.java
+++ platforming101/src/dev/apauley/display/Display.java
@@ -1,5 +1,7 @@
 package dev.apauley.display;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 //Displays game in a window
@@ -7,6 +9,7 @@ import javax.swing.JFrame;
 public class Display {
 
 	private JFrame frame;
+	private Canvas canvas; //Allows you to draw elements to the JFrame
 	
 	private String title;
 	private int width, height;
@@ -17,7 +20,6 @@ public class Display {
 		this.height = height;
 		
 		createDisplay();
-		
 	}
 	
 	private void createDisplay() {
@@ -27,6 +29,14 @@ public class Display {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width,height));
+		canvas.setMaximumSize(new Dimension(width,height));
+		canvas.setMinimumSize(new Dimension(width,height));
+		
+		frame.add(canvas);
+		frame.pack();
 	}
 	
 }
